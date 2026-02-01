401e768338c0979bfb00be5846799461d55c7e2e
4IP <4ipxxx@gmail.com>
2026-02-01 23:14:23 +0700

    fix: docs page no have UI/UX
    Signed-off-by: 4IP <4ipxxx@gmail.com>

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,4 +1,16 @@
 import Sidebar from "@/components/Sidebar";
+import "../globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
 
 export default function DocsLayout({
     children,
@@ -6,11 +18,15 @@ export default function DocsLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex min-h-screen">
-            <Sidebar />
-            <div className="flex-1">
-                {children}
-            </div>
-        </div>
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <div className="flex min-h-screen">
+                    <Sidebar />
+                    <div className="flex-1">
+                        {children}
+                    </div>
+                </div>
+            </body>
+        </html>
     );
 }
